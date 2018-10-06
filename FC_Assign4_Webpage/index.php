1de857dcfe8d0198b53802817b1c94d1076b970e
crookfion <crookfion@gs1608.AD.ECS.VUW.AC.NZ>
2018-10-06 21:13:47 +1300

    assign4 - styled a bit
    changed fonts and text sizes, added borders and colours, changes image
    sizes

--- FC_Assign4_Webpage/index.php
+++ FC_Assign4_Webpage/index.php
@@ -1,16 +1,10 @@
-<!-- have some php in here to read file, store image titles to array  -->
-
    
 <?php
-		$arTemp = file_get_contents("images.txt");
-		
-		$array = explode("\n", $arTemp,3); // not sure if need to limit this size 
-		
+		$arTemp = file_get_contents("images.txt");		
+		$array = explode("\n", $arTemp,3); // not sure if need to limit this size 		
 		$indexNum=file_get_contents("bigImageIndexNum.txt");
-		#$bigImageUrl = $array[$indexNum];
-		#$bigImageUrl = $array[$GET["index"]];  //not works
 		
-	?>  
+?>  
 	
 	
 <!DOCTYPE html>
@@ -23,53 +17,29 @@
 		</title>
 		
 	</head>
+	
 	<body>
-	<!-- look at what's in the array -->
 	
-	<?php
-	for ($i=0; $i<count($array); $i++) {
-		 echo $array[$i];
-		}
-		?>
-	
-	
-	<!--prob remove this later -->	
-	<div class="tiledBackground">
-	
-	</div>
-	
-		<h1>
-			Welcome to my Cat gallery
-		</h1>
-	
-	<p>
-	Filler text - browse gallery click image below to expand
-	</p>
-	
-	<!--  <div id="largeImage" ><img src="cat1.jpg" width="300" > </div><br/> -->
-	
-	<!-- resized image but still too big??  -->
-	<div class="bigImageLocate">
+	<h1>
+		Welcome to my Cat gallery
+	</h1>
 
-			<?php  
-			
-			$bigImageUrl =  $array[$indexNum]; 
-			?>  
-			
-		<img id="bigImage" >
-		<!--	<img src= <?php echo $bigImageUrl ?> class="bigImage"  >  -->
-			
-		<!-- <div class="bigImage"> </div> this was the original line-->
-		
+	<p class="text">
+	Browse the gallery below </br>
+	Click on an image to expand it
+	</p>
+
+
+	<div class="bigImageLocate">
+		<?php  $bigImageUrl =  $array[$indexNum]; ?>  			
+		<img src= <?php echo $bigImageUrl ?> id="bigImage"  > 
 	</div>
 	
 	<!-- trying to access array, and create an article for each element, of class smallImage -->
 	
 	<section class="imageGallery">
-		<?php for($i=0;$i<count($array);$i++){ 
-			 ?>
-			 
-			 <img src= <?php echo $array[$i]?> class="smallImage"  onclick="expandImage(this);">  <!-- jump to php here because the array only exists inside php mode  -->
+		<?php for($i=0;$i<count($array);$i++){  ?>			 
+			 <img src= <?php echo $array[$i]?> class="smallImage"  onclick="expandImage(this);">  
 		<?php  }  ?>
 	</section>
 	
--- FC_Assign4_Webpage/main.js
+++ FC_Assign4_Webpage/main.js
@@ -1,59 +1,6 @@
-//function enablePostBack()
-//	{
-        //T1 is the first argument(name of our control) I mentioned earlier and give the
-       // value of second argument as "" that's all
- //      __doPostBack('indexNum',"");
-//	}
-
-//import System.IO; // complaining here
-
-//function Start () {
-    // Create an instance of StreamWriter to write text to a file.
-//    sw = new StreamWriter("bigImageIndexNum.txt");
-    // Add some text to the file.
-//    sw.Write(indexNum);
-//    sw.Close();
-//}
-
-let ar=document.querySelectorAll(".smallImage")
-for(let indexNum=0;indexNum<ar.length;indexNum++){
-	
-	ar[indexNum].addEventListener('click', () => {
-		console.log(ar[indexNum]);   //prints the <img src="cat4.jpg" class="smallImage"> info
-		console.log(indexNum); //prints the array number
-		//console.log(array[indexNum]); //array is undefined
-		
-		//__doPostBack('indexNum');
-		//enablePostBack();
-		//Start();
-		//window.location.reload(forceGet);
-		//window.location.href("index.php?index="+indexNum);
-		//expandImage(ar[indexNum]);
-	}
-	)
-	}
 	
 function expandImage(imgs){
 	var expandImg = document.getElementById("bigImage");
 	expandImg.src=imgs.src;
-	expandImg.parentElement.style.display="block";
+	//expandImg.parentElement.style.display="block";
 }
-	
-//function myFunction(imgs) {
-
-  // Get the expanded image
-//  var expandImg = document.getElementById("expandedImg");
-
-  // Get the image text
-//  var imgText = document.getElementById("imgtext");
-  
-  // Use the same src in the expanded image as the image being clicked on from the grid
-//  expandImg.src = imgs.src;
-
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
-//  imgText.innerHTML = imgs.alt;
-
-  // Show the container element (hidden with CSS)
-//  expandImg.parentElement.style.display = "block";
-//}
-	
