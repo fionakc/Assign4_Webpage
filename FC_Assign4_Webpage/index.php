edddba4caa8f91eed2c92a43558d114d0876dc5f
crookfion <crookfion@gs1608.AD.ECS.VUW.AC.NZ>
2018-10-07 18:00:35 +1300

    Assign4 - small tweaks
    Mostly code tidy, add some comments, add in more small images

--- FC_Assign4_Webpage/index.php
+++ FC_Assign4_Webpage/index.php
@@ -1,9 +1,14 @@
-   
+<!--
+SWEN502 - Webpages Assignment
+Fiona Crook
+300442873
+-->
+
+<!-- read in the picture urls to an array, and load index of first image into var indexNum -->
 <?php
-		$arTemp = file_get_contents("images.txt");		
-		$array = explode("\n", $arTemp,3); // not sure if need to limit this size 		
-		$indexNum=file_get_contents("bigImageIndexNum.txt");
-		
+	$arTemp = file_get_contents("images.txt");		
+	$array = explode("\n", $arTemp); // not sure if need to limit this size 		
+	$indexNum=file_get_contents("bigImageIndexNum.txt");	
 ?>  
 	
 	
@@ -13,7 +18,7 @@
 		<meta charset="utf-8" />
 		<link rel="stylesheet" href="style.css" />
 		<title>
-			Gallery
+			My Cat Gallery
 		</title>
 		
 	</head>
@@ -29,14 +34,14 @@
 	Click on an image to expand it
 	</p>
 
-
+	<!-- create container and load first big image, using var indexNum -->
 	<div class="bigImageLocate">
 		<?php  $bigImageUrl =  $array[$indexNum]; ?>  			
 		<img src= <?php echo $bigImageUrl ?> id="bigImage"  > 
 	</div>
+	</br>
 	
-	<!-- trying to access array, and create an article for each element, of class smallImage -->
-	
+	<!-- cycle through array, create gallery of smaller image 'thumbnails' -->
 	<section class="imageGallery">
 		<?php for($i=0;$i<count($array);$i++){  ?>			 
 			 <img src= <?php echo $array[$i]?> class="smallImage"  onclick="expandImage(this);">  
--- FC_Assign4_Webpage/main.js
+++ FC_Assign4_Webpage/main.js
@@ -1,6 +1,9 @@
+//SWEN502 - Webpages Assignment
+//Fiona Crook
+//300442873
 	
+//When click on the small image, load it into the big image space
 function expandImage(imgs){
 	var expandImg = document.getElementById("bigImage");
 	expandImg.src=imgs.src;
-	//expandImg.parentElement.style.display="block";
 }
